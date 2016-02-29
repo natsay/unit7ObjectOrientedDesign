2b5bc08ea0a6162a2178ad6e3998c7095d9278a7
natsay <nhtse@NN123-HA-20.HS.sd203.org>
2016-02-29 10:28:15 -0600

    2/29/16

--- DrawingEditor/ControlPanel.java
+++ DrawingEditor/ControlPanel.java
@@ -1,11 +1,18 @@
 import javax.swing.JPanel; 
 import javax.swing.JButton; 
+import javax.swing.JLabel;
+import java.awt.Color; 
+import javax.swing.JColorChooser;
+import java.awt.event.ActionEvent; 
+import java.awt.event.ActionListener; 
 public class ControlPanel extends JPanel 
 {
     private JPanel currentColor; 
     private JButton color;
     private JButton circle;
     private JButton square; 
+
+    private JLabel sampleText = new JLabel("Label");
     
    
     /**
@@ -19,6 +26,8 @@ public class ControlPanel extends JPanel
         this.circle= new JButton("Add Circle"); 
         this.square= new JButton("Add Square"); 
         
+        color.addActionListener(new ButtonListener()); 
+       
         this.add(this.color); 
         this.add(this.circle); 
         this.add(this.square); 
@@ -26,7 +35,21 @@ public class ControlPanel extends JPanel
        
         
     }
+    
+    public class ButtonListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e) 
+        {
+             Color c = JColorChooser.showDialog(null, "Choose a Color", sampleText.getForeground());
+             if (c != null)
+             {
+                 sampleText.setForeground(c);
+                 currentColor.setBackground(c);
+             }    
+             
+        }
+    }     
+}
 
   
 
-}
--- DrawingEditor/DrawingPanel.java
+++ DrawingEditor/DrawingPanel.java
@@ -1,14 +1,69 @@
 import java.awt.Graphics2D; 
+import java.awt.event.MouseEvent; 
+import java.awt.event.MouseListener; 
 import java.awt.Color;
 import javax.swing.JPanel;
+import java.util.ArrayList; 
 public class DrawingPanel extends JPanel 
 {
-        
+//     private ArrayList<Shape> shape; 
+//     private Shape active; 
+    private JPanel panel; 
     public DrawingPanel()
     {
-        
-        
+        this.setBackground(Color.WHITE); 
+       
     }  
-
     
+//     public getColor() 
+//     {
+//         //
+//     }
+//     
+//     public pickColor() 
+//     {
+//         //
+//     }
+//     
+//     public addCircle() 
+//     {
+//         //
+//     }
+//     
+//     public addSquare()
+//     {
+//         //
+//     }
+//     
+//     public paintComponent(Graphics g)
+//     {
+//         //
+//     }
+//     public class MouseListener implements MouseListener
+//     {
+//         public void mousePressed(MouseEvent event) 
+//         {
+//         }
+//         public void mouseDragged(MouseEvent event) 
+//         {
+//         }
+//         public void keyPresssed(MouseEvent event) 
+//         {
+//         }
+//         
+//     }
+//     
+//     public class MouseMotionListener implements MouseMotionListener 
+//     {
+//         public void mousePressed(MouseEvent event) 
+//         {
+//         }
+//         public void mouseDragged(MouseEvent event) 
+//         {
+//         }
+//         public void keyPresssed(MouseEvent event) 
+//         {
+//         }
+//     }
+       
 }
